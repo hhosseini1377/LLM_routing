587fa5ca7234647d3c3775955408c5020a11b282
hhosseini1377 <h.hosseinik99@gmail.com>
2025-07-10 12:45:38 +1000

    jobs done

--- job.sh
+++ job.sh
@@ -2,9 +2,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
 #SBATCH --time=0-12:00:00
-#SBATCH -p gpu-h100
+#SBATCH -p gpu-a100
 #SBATCH --gres=gpu:1
-#SBATCH --mem=64G
+#SBATCH --mem=24G
 
 # Activate your environment
 # source ~/envs/llm-routing/bin/activate
@@ -13,6 +13,6 @@
 module purge
 module load GCCcore/11.3.0
 module load Python/3.10.4
-source ~/venvs/venv-3.10.4/bin/activate
+source ./vnv/bin/activate
 
 python train_BERT.py --model deberta
